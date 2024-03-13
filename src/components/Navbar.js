9b161a1b08ca3841d33c4d6444dce118bbc66931
ManavX <71044844+ManavX@users.noreply.github.com>
2024-03-13 15:31:35 -0700

    Update Navbar.js (notification sound)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../services/firebase";
 import { LogoutIcon, BellIcon } from "@heroicons/react/outline";
 
